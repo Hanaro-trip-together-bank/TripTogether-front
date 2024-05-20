8b5b8179d700ac6a2b488545998265722bd3f0eb
kwang <shinkwang@yonsei.ac.kr>
2024-05-20 11:04:02 +0900

    init: Setting project

---
+++ .storybook/preview.ts
@@ -0,0 +1,36 @@
+import '../src/index.css';
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'secondary',
+      values: [
+        {
+          name: 'gray',
+          value: '#dddddd',
+        },
+        {
+          name: 'white',
+          value: '#ffffff',
+        },
+        {
+          name: 'black',
+          value: '#000000',
+        },
+        {
+          name: 'secondary',
+          value: '#353a4e',
+        },
+      ],
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500">테스트 해볼까요</div>
+    </>
+  );
+}
+
+export default App;
