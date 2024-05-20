2f562294d75173dade9636a9c404fe340f218651
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-20 13:36:46 +0900

    feat: add VStack, HStack, Spacer components and stories #1

---
+++ src/stories/common/HStack.stories.ts
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "../../components/common/Button";
+import { HStack, Spacer } from "../../components/common/Stack";
+
+const meta = {
+  title: "common/HStack",
+  component: HStack,
+  parameters: {
+    layout: "centered",
+  },
+  args: {},
+} satisfies Meta<typeof HStack>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: [
+      Button({ children: "1" }),
+      Button({ children: "2" }),
+      Button({ children: "3" }),
+      Button({ children: "4" }),
+    ],
+  },
+};
+export const Rotated: Story = {
+  args: {
+    rotated: true,
+    children: [
+      Button({ children: "1" }),
+      Button({ children: "2" }),
+      Button({ children: "3" }),
+      Button({ children: "4" }),
+    ],
+  },
+};
+export const withSpacer: Story = {
+  args: {
+    className: "w-96",
+    rotated: false,
+    children: [
+      Button({ children: "1" }),
+      Button({ children: "2" }),
+      Spacer(),
+      Button({ children: "4" }),
+    ],
+  },
+};
