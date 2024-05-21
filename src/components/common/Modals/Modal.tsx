14751a1d1bef9e2557c5ff6cdec916b08c241c3f
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-21 11:21:54 +0900

    fix: 모달이 완전히 사라지지 않아 클릭 방해하던 문제, 키보드 배열 자꾸 바뀌던 문제 #1

--- src/components/common/Modals/Modal.tsx
+++ src/components/common/Modals/Modal.tsx
@@ -32,7 +32,7 @@ function Modal({
   // 기본 백드롭 클래스네임
   const baseBackDropClassName = "transition-opacity";
   // 백드롭 투명도 처리
-  const backDropOpacityClassName = show ? "opacity-100" : "opacity-0";
+  const backDropOpacityClassName = showContent ? "opacity-100" : "opacity-0";
   // 백드롭 클래스네임
   const processedBackDropClassName = cn(
     baseBackDropClassName,
@@ -42,79 +42,90 @@ function Modal({
   // 애니메이션 고려해서 모달/시트 열 때는 바로 컨텐츠 보여주지만
   // 닫을 때는 0.2초 지연 후 가림
   useEffect(() => {
-    let hideContent: string | number | NodeJS.Timeout | undefined;
     if (show) setShowContent(true);
-    else hideContent = setTimeout(() => setShowContent(false), 200);
-    return () => {
-      clearTimeout(hideContent);
-    };
+    else {
+      const hideContent = setTimeout(() => setShowContent(false), 200);
+      return () => {
+        clearTimeout(hideContent);
+      };
+    }
   }, [show]);
 
-  return (
-    <div className={cn("absolute w-full", show ? "h-full" : "h-0 delay-100")}>
-      {/* 백드롭(어두운 오버레이) + 상단 스테이터스바 흰색버전 */}
-      <div className={processedBackDropClassName}>
-        <div className="absolute w-full z-40">
-          <StatusBar white />
-        </div>
-        <div
-          className="absolute w-full h-full bg-black opacity-50 z-30"
-          onClick={() => {
-            if (backDrop) onClose();
-          }}
-        />
-        <div className="absolute w-full h-full flex flex-col items-center justify-center">
-          {
-            // 모달 또는 시트 렌더링
+  if (show || showContent)
+    return (
+      <div
+        className={cn(
+          "absolute w-iPhone h-iPhone",
+          showContent ? "" : "delay-100"
+        )}
+      >
+        {/* 백드롭(어두운 오버레이) + 상단 스테이터스바 흰색버전 */}
+        <div className={processedBackDropClassName}>
+          <div className="absolute w-iPhone px-0.5">
+            <StatusBar white />
+          </div>
+          <div
+            className="absolute w-iPhone h-iPhone bg-black opacity-50 z-30 rounded-3xl"
+            onClick={() => {
+              if (backDrop) onClose();
+            }}
+          />
+          <div className="absolute w-iPhone h-iPhone flex flex-col items-center justify-center">
             {
-              // 모달
-              modal: (
-                <div
-                  className={cn(
-                    "absolute p-8 shadowed rounded-3xl w-fit h-fit z-40 transition-all delay-100",
-                    show ? "opacity-100" : "translate-y-10 opacity-0",
-                    dark ? "bg-secondary" : "bg-white",
-                    className
-                  )}
-                  {...props}
-                >
-                  {xButton && (
-                    <XButton
-                      className="absolute -translate-x-4 -translate-y-4"
-                      onClick={onClose}
-                      white={dark}
-                    />
-                  )}
-                  {showContent && children}
-                </div>
-              ),
-              // 시트
-              sheet: (
-                <VStack
-                  className={cn(
-                    "absolute bottom-0 w-full h-fit p-6 rounded-t-3xl z-40 transition-all delay-100",
-                    show ? "opacity-100" : "translate-y-20 opacity-0",
-                    dark ? "bg-secondary" : "bg-white",
-                    className
-                  )}
-                  {...props}
-                >
-                  {xButton && (
-                    <XButton
-                      className="absolute -translate-x-4 -translate-y-4"
-                      onClick={onClose}
-                      white={dark}
-                    />
-                  )}
-                  {showContent && children}
-                </VStack>
-              ),
-            }[modalType]
-          }
+              // 모달 또는 시트 렌더링
+              {
+                // 모달
+                modal: (
+                  <div
+                    className={cn(
+                      "absolute p-8 shadowed rounded-3xl w-fit h-fit z-40 transition-all delay-100",
+                      show && showContent
+                        ? "opacity-100"
+                        : "translate-y-10 opacity-0",
+                      dark ? "bg-secondary" : "bg-white",
+                      className
+                    )}
+                    {...props}
+                  >
+                    {xButton && (
+                      <XButton
+                        className="absolute -translate-x-4 -translate-y-4"
+                        onClick={onClose}
+                        white={dark}
+                      />
+                    )}
+                    {children}
+                  </div>
+                ),
+                // 시트
+                sheet: (
+                  <VStack
+                    className={cn(
+                      "absolute bottom-0 w-iPhone h-fit p-6 rounded-t-3xl z-40 transition-all delay-100",
+                      show && showContent
+                        ? "opacity-100"
+                        : "translate-y-20 opacity-0",
+                      dark ? "bg-secondary" : "bg-white",
+                      className
+                    )}
+                    {...props}
+                  >
+                    {xButton && (
+                      <XButton
+                        className="absolute -translate-x-4 -translate-y-4"
+                        onClick={onClose}
+                        white={dark}
+                      />
+                    )}
+                    {children}
+                  </VStack>
+                ),
+              }[modalType]
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Modal;
