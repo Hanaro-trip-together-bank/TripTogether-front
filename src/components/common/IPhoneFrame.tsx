93a68e3d5580d854197101f9a603bcc8ea0bb1e6
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-25 14:58:37 +0900

    feat: 모바일에서는 상단 스테이터스 바 없앰 #1

--- src/components/common/IPhoneFrame.tsx
+++ src/components/common/IPhoneFrame.tsx
@@ -5,7 +5,7 @@ import cn from "../../utils/cn";
 interface IPhoneFrameProps extends HTMLAttributes<HTMLDivElement> {}
 function IPhoneFrame({ className = "", children, ...props }: IPhoneFrameProps) {
   const baseClassName =
-    "relative w-iPhone h-iPhone iPhone:mt-2 iPhone:rounded-3xl iPhone:border-2 border-black overflow-hidden items-center !gap-0 box-content";
+    "relative w-iPhone h-iPhone shadowed -translate-y-12 iPhone:translate-y-0 iPhone:mt-2 iPhone:rounded-3xl iPhone:border-2 border-black overflow-hidden items-center !gap-0 box-content";
   const processedClassName = cn(baseClassName, className);
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
