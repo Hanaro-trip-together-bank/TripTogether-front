75c85281d6f0775b8431ee0f1c99b02e09309b07
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-28 14:24:47 +0900

    feat: 체크서클 #6

--- src/pages/Main/LoginPage.tsx
+++ src/pages/Main/LoginPage.tsx
@@ -5,6 +5,8 @@ import { useNavigation } from "../../contexts/useNavigation";
 import Keypad from "../../components/common/Modals/Keypad";
 import cn from "../../utils/cn";
 import usePassword from "../../hooks/usePassword";
+import Check from "../../components/common/Check";
+import useToggle from "../../hooks/useToggle";
 
 interface LoginPageProps {
   onLoginDone: () => void;
@@ -13,6 +15,7 @@ interface LoginPageProps {
 function LoginPage({ onLoginDone }: LoginPageProps) {
   const { back } = useNavigation();
   const { password, append, remove } = usePassword();
+  const [autoLogin, toggleAutoLogin] = useToggle();
   useEffect(() => {
     if (password.length == 6) {
       // TODO: 로그인 로직
@@ -36,10 +39,14 @@ function LoginPage({ onLoginDone }: LoginPageProps) {
             />
           ))}
         </HStack>
-        <HStack className="gap-2 text-gray-500 text-sm">
+        <HStack className="gap-2 text-gray-500 text-sm mb-4">
           <span className="underline">비밀번호 초기화</span>
           <span className="">|</span>
           <span className="underline">다른 로그인방법</span>
+        </HStack>
+        <HStack className="items-center text-gray-500 gap-2">
+          <Check checked={autoLogin} onClick={toggleAutoLogin} />
+          <span>자동 로그인</span>
         </HStack>
         {/* TODO: 체크, 자동로그인, (?) */}
       </VStack>
