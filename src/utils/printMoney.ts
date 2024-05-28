7b3ab45ebf7a067a16cf5b3485aa4b35a72475f1
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-28 11:33:08 +0900

    feat: useToggle, printMoney #6

---
+++ src/utils/printMoney.ts
@@ -0,0 +1,15 @@
+export const printMoney = (money: number) => {
+  const units = ["", "만", "억", "조", "경", "해"];
+  let strNumber = money.toString();
+  let result = "";
+  let unitIndex = 0;
+
+  while (strNumber.length > 4) {
+    const chunk = strNumber.slice(-4);
+    strNumber = strNumber.slice(0, -4);
+    result = (+chunk).toLocaleString() + units[unitIndex] + (result ?? "");
+    unitIndex++;
+  }
+  result = (+strNumber).toLocaleString() + units[unitIndex] + (result ?? "");
+  return result;
+};
