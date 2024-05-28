7d3e7dd3409fe60bfb4ed9971ce41f44195386a0
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-28 10:13:06 +0900

    feat: 공통 컴포넌트 시각적 수정, 네비게이션 링크/네비게이션 바 확장성 개선 #6

--- src/components/common/Arrow.tsx
+++ src/components/common/Arrow.tsx
@@ -18,8 +18,20 @@ function Arrow({ direction }: ArrowProps) {
     }
   })();
   return (
-    <button className={cn("text-gray-500 p-2 transition-all z-0", rotation)}>
-      ^
+    <button className={cn("text-gray-500 transition-all z-0", rotation)}>
+      <svg
+        width="16px"
+        height="16px"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10 60 L50 20 L90 60"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="8"
+        />
+      </svg>
     </button>
   );
 }
--- src/components/common/Navigation/NavigationLink.tsx
+++ src/components/common/Navigation/NavigationLink.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import React, { PropsWithChildren, cloneElement } from "react";
 import { useNavigation } from "../../../contexts/useNavigation";
 import Route from "../../../types/Route";
 
@@ -13,6 +13,15 @@ function NavigationLink({
   children,
 }: PropsWithChildren<NavigationLinkProps>) {
   const { navigateTo } = useNavigation();
+
+  //만약 자식이 버튼이라면 onClick 속성만 추가함
+  if (React.isValidElement(children) && children.type === "button") {
+    const childWithOnClick = cloneElement(children, {
+      onClick: () => navigateTo(to),
+    } as React.HTMLProps<HTMLButtonElement>);
+    return childWithOnClick;
+  }
+
   return (
     <button className={className} onClick={() => navigateTo(to)}>
       {children}
