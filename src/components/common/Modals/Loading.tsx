479a506690bfb7ed1709ebb5f1a523946bffc66c
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-30 20:53:02 +0900

    chore: 로딩 오버레이 약간 수정 #14
    chore: 로딩 오버레이 약간 수정 #14

--- src/components/common/Modals/Loading.tsx
+++ src/components/common/Modals/Loading.tsx
@@ -1,5 +1,5 @@
+import { HStack, VStack } from "../Stack";
 import cn from "../../../utils/cn";
-import { HStack } from "../Stack";
 
 interface LoadingProps {
   show: boolean;
@@ -8,16 +8,19 @@ interface LoadingProps {
 
 function Loading({ show, label = "" }: LoadingProps) {
   return (
-    <HStack
+    <VStack
       className={cn(
         "absolute top-0 left-0 w-iPhone h-iPhone z-30 rounded-3xl transition-opacity justify-center items-center  backdrop-blur-sm bg-black/50",
-        show ? "pointer-events-auto" : "pointer-events-none opacity-0"
+        show ? "pointer-events-auto" : "pointer-events-none opacity-0 delay-150"
       )}
     >
-      <div className="w-3 h-3 rounded-full bg-primary animate-[bounce2_1s_infinite_000ms]" />
-      <div className="w-3 h-3 rounded-full bg-primary animate-[bounce2_1s_infinite_100ms]" />
-      <div className="w-3 h-3 rounded-full bg-primary animate-[bounce2_1s_infinite_200ms]" />
-    </HStack>
+      <HStack>
+        <div className="w-3 h-3 rounded-full bg-primary animate-[bounce2_1s_infinite_000ms]" />
+        <div className="w-3 h-3 rounded-full bg-primary animate-[bounce2_1s_infinite_100ms]" />
+        <div className="w-3 h-3 rounded-full bg-primary animate-[bounce2_1s_infinite_200ms]" />
+      </HStack>
+      <span className="text-white">{label}</span>
+    </VStack>
   );
 }
 
