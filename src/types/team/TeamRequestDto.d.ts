ca3d6da1766514bf1ac8560b56d5c84e621a0a3b
yubin-im <ybyb130@gmail.com>
2024-05-30 16:19:59 +0900

    feat: Team DTO 타입 정의

---
+++ src/types/team/TeamRequestDto.d.ts
@@ -0,0 +1,27 @@
+export type AddTeamReqDto = {
+  memberIdx: number;
+  accIdx: number;
+  teamType: string;
+  teamName: string;
+  preferenceType: string;
+};
+
+export type ExportTeamReqDto = {
+  teamIdx: number;
+  member: number;
+};
+
+export type InviteTeamReqDto = {
+  memberIdx: number;
+  teamIdx: number;
+};
+
+export type ManageTeamReqDto = {
+  teamIdx: number;
+  memberIdx: number;
+};
+
+export type UpdateTeamNoticeReq = {
+  teamIdx: number;
+  teamNotice: string;
+};
