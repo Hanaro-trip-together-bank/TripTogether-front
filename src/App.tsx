452cd71830a2c2e8f031146da1a75c39a70ac39f
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-30 20:50:58 +0900

    feat: AuthenticationContext 작성, 로그인 테스트 완료 #14

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import Alarm from "./components/common/Alarm";
 import IPhoneFrame from "./components/common/IPhoneFrame";
 import NavigationStack from "./components/common/Navigation/NavigationStack";
 import Toggle from "./components/common/Toggle";
-import { NavigationProvider } from "./contexts/useNavigation";
+import { useAuth } from "./contexts/useAuth";
 import useToggle from "./hooks/useToggle";
 
 function App() {
   const [showAlarm, toggleAlarm] = useToggle();
+  const { member, setIdx } = useAuth();
   return (
     <>
       <Toggle selected={showAlarm} onClick={toggleAlarm} />
-      <NavigationProvider>
-        <IPhoneFrame>
-          <Alarm show={showAlarm} />
-          <NavigationStack />
-        </IPhoneFrame>
-      </NavigationProvider>
+      <input
+        className="w-12"
+        type={"number"}
+        value={member.memberIdx}
+        onChange={(e) => setIdx(+e.target.value)}
+      />
+      <IPhoneFrame>
+        <Alarm show={showAlarm} />
+        <NavigationStack />
+      </IPhoneFrame>
     </>
   );
 }
---
+++ src/types/member/Member.d.ts
@@ -0,0 +1,4 @@
+export type Member = {
+  memberIdx: number;
+  memberName?: string; //로그인하면 채워짐
+};
