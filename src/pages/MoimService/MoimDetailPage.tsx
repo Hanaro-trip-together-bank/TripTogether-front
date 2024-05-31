f0c2f074d4b4f89d9e72758865bffb1cbc936436
lcw729 <lcw729729@gmail.com>
2024-05-31 15:34:39 +0900

    feat: 회비 낸 사람/안 낸 사람 조회

--- src/pages/MoimService/MoimDetailPage.tsx
+++ src/pages/MoimService/MoimDetailPage.tsx
@@ -9,13 +9,16 @@ import NavigationBar from "../../components/common/TopBars/NavigationBar";
 import Arrow from "../../components/common/Arrow";
 import NavigationLink from "../../components/common/Navigation/NavigationLink";
 import MoimDepositPage from "./Dues/MoimDepositPage";
-import MoimDeusMainPage from "./Dues/MoimDuesMainPage";
 import Avatar from "../../components/common/Avatar";
 import MoimMembersMainPage from "./Members/MoimMembersMainPage";
 import MoimTripsMainPage from "./Trips/MoimTripsMainPage";
-interface MoimDetailPageProps {}
+import MoimDuesMainPage from "./Dues/MoimDuesMainPage";
+interface MoimDetailPageProps {
+  teamIdx: number;
+  accIdx:  number;
+}
 
-function MoimDetailPage({}: MoimDetailPageProps) {
+function MoimDetailPage({ accIdx, teamIdx }: MoimDetailPageProps) {
   const [notice, setNotice] = useState<string>("");
   const [showNoticeEdit, setShowNoticeEdit] = useState(false);
 
@@ -91,7 +94,7 @@ function MoimDetailPage({}: MoimDetailPageProps) {
         <HStack className="justify-evenly my-4">
           <VStack>
             <NavigationLink
-              to={{ page: <MoimDeusMainPage /> }}
+              to={{ page: <MoimDuesMainPage accIdx={1} teamIdx={1} /> }}
               className="bg-gray-100 flex items-center justify-center shadowed rounded-xl w-16 h-16"
             >
               <img
--- src/utils/urlFactory.ts
+++ src/utils/urlFactory.ts
@@ -12,3 +12,16 @@ export const TripsPostURL = () => `${BASE_URL}/trips`;
 
 // GET - 여행 상세 정보 읽기: () -> TripResDto
 export const TripsGetURL = (tripIdx: number) => `${BASE_URL}/trips/${tripIdx}`;
+
+export const DuesGetRuleURL = (teamIdx: number) =>
+  `${BASE_URL}/dues/rule/${teamIdx}`;
+
+// /dues?paid=false&date=2024-05&accIdx=1&teamIdx=1
+export const DuesGetStatusUrl = (
+  year: number,
+  month: number,
+  status: boolean,
+  accIdx: number,
+  teamIdx: number
+) =>
+  `${BASE_URL}/dues?paid=${status}&date=${year}-${month.toString().padStart(2, "0")}&accIdx=${accIdx}&teamIdx=${teamIdx}`;
