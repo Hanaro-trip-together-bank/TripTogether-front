48ae482691cecc15da8dd53fb1be6106fc4e9b2e
wldnd9904 <wldnd9904@uos.ac.kr>
2024-06-07 15:47:07 +0900

    feat: 여행 삭제

--- src/pages/MoimService/Trips/MoimTripsMainPage.tsx
+++ src/pages/MoimService/Trips/MoimTripsMainPage.tsx
@@ -9,8 +9,15 @@ import cn from "../../../utils/cn";
 import MoimTripDetailPage from "./MoimTripDetailPage";
 import TripImage from "../../../components/trip/TripImg";
 import { useFetch } from "../../../hooks/useFetch";
-import { PreferTripPutURL, TeamTripsGetURL } from "../../../utils/urlFactory";
-import { TripListResDto } from "../../../types/trip/TripResponseDto";
+import {
+  PreferTripPutURL,
+  TeamTripsGetURL,
+  TipsDeleteURL,
+} from "../../../utils/urlFactory";
+import {
+  TripListResDto,
+  TripResDto,
+} from "../../../types/trip/TripResponseDto";
 import Loading from "../../../components/common/Modals/Loading";
 import getPeriod from "../../../utils/getPeriod";
 import getDaysRemaining from "../../../utils/getDaysRemaining";
@@ -18,6 +25,7 @@ import { useFetchTrigger } from "../../../hooks/useFetchTrigger";
 import { PreferTripReqDto } from "../../../types/team/TeamRequestDto";
 import { useAuth } from "../../../contexts/useAuth";
 import CreateTripPage from "../../CreateTripPage";
+import { useModal } from "../../../hooks/useModal";
 
 interface MoimTripsMainPageProps {
   teamIdx: number;
@@ -31,6 +39,27 @@ function MoimTripsMainPage({
   const { member } = useAuth();
   const [showNewTripModal, toggleShowNewTripModal] = useToggle();
   const [animationStarted, setAnimationStarted] = useState(false);
+  const [selectedTrip, setSelectedTrip] = useState<TripResDto>();
+
+  const tripDeleteFetcher = useFetchTrigger(
+    TipsDeleteURL(selectedTrip?.tripIdx ?? 0),
+    "DELETE"
+  );
+
+  const { modal: deletedModal, triggerModal: triggerDeletedModal } = useModal(
+    `${selectedTrip?.tripName ?? "여행"} 계획을 삭제했습니다.`,
+    () => {
+      refetch();
+    },
+    false
+  );
+  const { modal: deleteModal, triggerModal: triggerDeleteModal } = useModal(
+    `정말로 ${selectedTrip?.tripName ?? "여행"} 계획을 삭제할까요?`,
+    () => {
+      tripDeleteFetcher.trigger(null);
+      triggerDeletedModal();
+    }
+  );
   const { data, isLoading, refetch } = useFetch<null, TripListResDto>(
     TeamTripsGetURL(teamIdx),
     "GET"
@@ -121,25 +150,28 @@ function MoimTripsMainPage({
                 </VStack>
                 <VStack>
                   {data?.preferTripIdx == trip.tripIdx ? (
-                    // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
-                    <span
+                    <button
                       className="text-xl text-yellow-300"
                       onClick={() => requestDeletePrefer()}
                     >
                       ★
-                    </span>
+                    </button>
                   ) : (
-                    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-                    <span
+                    <button
                       className="text-xl"
                       onClick={() => requestPrefer(trip.tripIdx)}
                     >
                       ☆
-                    </span>
+                    </button>
                   )}
-                  <span className="text-gray-500 underline text-sm text-nowrap">
-                    관리
-                  </span>
+                  <button
+                    onClick={() => {
+                      setSelectedTrip(trip);
+                      triggerDeleteModal();
+                    }}
+                  >
+                    <TrashCan />
+                  </button>
                 </VStack>
               </HStack>
               <HStack className="relative justify-center h-52 text-primary">
@@ -211,8 +243,28 @@ function MoimTripsMainPage({
         </VStack>
       </Modal>
       <Loading show={isLoading} label="여행 목록을 조회하는 중 ..." />
+      {deleteModal}
+      {deletedModal}
     </>
   );
 }
 
 export default MoimTripsMainPage;
+
+function TrashCan() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+    >
+      <path d="M9.1709 4C9.58273 2.83481 10.694 2 12.0002 2C13.3064 2 14.4177 2.83481 14.8295 4"></path>
+      <path d="M20.5001 6H3.5" stroke="#1C274C"></path>
+      <path d="M18.8332 8.5L18.3732 15.3991C18.1962 18.054 18.1077 19.3815 17.2427 20.1907C16.3777 21 15.0473 21 12.3865 21H11.6132C8.95235 21 7.62195 21 6.75694 20.1907C5.89194 19.3815 5.80344 18.054 5.62644 15.3991L5.1665 8.5"></path>
+      <path d="M9.5 11L10 16"></path> <path d="M14.5 11L14 16"></path>
+    </svg>
+  );
+}
