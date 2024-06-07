6cc7fbb7e9e499888b0de0b4952dc913e4add8de
안나영 <dpswpfskdud@naver.com>
2024-06-07 10:12:34 +0900

    refactor: firebase-messaging-sw.js 수정 #65

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,13 +7,10 @@ importScripts(
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 const firebaseApp = firebase.initializeApp({
-  apiKey: import.meta.env.VITE_FCM_API_KEY,
-  authDomain: import.meta.env.VITE_FCM_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FCM_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FCM_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FCM_APP_ID,
-  measurementId: import.meta.env.VITE_FCM_MEASUREMENT_ID,
+  apiKey: "AIzaSyDQtHhFZZbld2hgmgmSzwEcTMnv45RA5HA",
+  projectId: "triptogether-e7bac",
+  messagingSenderId: "920408874636",
+  appId: "1:920408874636:web:b46861c88c2123410425a9",
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
